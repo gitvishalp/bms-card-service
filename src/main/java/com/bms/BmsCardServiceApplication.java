b9e279554c748b30b063257e6fe287fc19419117
vishal-cqs <vishal@cqs.in>
2024-02-19 17:14:37 +0530

    initializing files

---
+++ src/main/java/com/bms/BmsCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsCardServiceApplication.class, args);
+	}
+
+}
